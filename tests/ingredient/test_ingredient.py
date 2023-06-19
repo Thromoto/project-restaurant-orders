28aa74ca3b7c271b41005db0df176369e93a427c
Thiago Omoto <thi.omoto@gmail.com>
2023-06-19 14:54:13 -0300

    testando classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ing_farinha = Ingredient("farinha")
+    ing_manteiga = Ingredient("manteiga")
+    ing_manteiiga = Ingredient("manteiga")
+
+    assert ing_farinha.name == "farinha"
+
+    assert ing_manteiga == ing_manteiiga
+
+    assert hash(ing_manteiga) == hash(ing_manteiiga)
+    assert hash(ing_farinha) != hash(ing_manteiga)
+
+    assert repr(ing_manteiga) == "Ingredient('manteiga')"
+
+    assert ing_manteiga.restrictions == {
+        Restriction.LACTOSE, Restriction.ANIMAL_DERIVED
+        }
